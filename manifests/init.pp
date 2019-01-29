62fa30c721bde96b80a963e1326fdd967040d36d
hugo.deprez <hugo.deprez@sodexo.com>
2019-01-29 19:35:42 +0100

    Make apt_proxy variable optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,20 @@ class squash_tm (
 ){
 
 
-
-  apt::key { 'squash-tm':
-    id      => '071D30C5671A11F9FC029283417A5213D56D74C7',
-    server  => 'pgp.mit.edu',
-    options => "http-proxy=$apt_proxy",
+  if $apt_proxy {
+    apt::key { 'squash-tm':
+      id      => '071D30C5671A11F9FC029283417A5213D56D74C7',
+      server  => 'pgp.mit.edu',
+      options => "http-proxy=$apt_proxy",
+    }
   }
+  else {
+    apt::key { 'squash-tm':
+      id      => '071D30C5671A11F9FC029283417A5213D56D74C7',
+      server  => 'pgp.mit.edu',
+    }
+  }
+
 
   package{ 'squash-tm':
     ensure => present,
