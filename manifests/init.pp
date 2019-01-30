9be7aa8e146f71ec4f1131bccf6b473853e89e53
hugo.deprez <hugo.deprez@sodexo.com>
2019-01-30 10:40:13 +0100

    Add variable for apt-key server

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,20 @@
 class squash_tm (
   Optional[String] $apt_proxy = undef,
-
+  String $apt_key_server      = "hkp://keyserver.ubuntu.com:80",
 ){
 
 
   if $apt_proxy {
     apt::key { 'squash-tm':
       id      => '071D30C5671A11F9FC029283417A5213D56D74C7',
-      server  => 'pgp.mit.edu',
+      server  => $apt_key_server,
       options => "http-proxy=$apt_proxy",
     }
   }
   else {
     apt::key { 'squash-tm':
       id      => '071D30C5671A11F9FC029283417A5213D56D74C7',
-      server  => 'pgp.mit.edu',
+      server  => $apt_key_server,
     }
   }
 
